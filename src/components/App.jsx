b0f7af9ec6e8b2a2ff44a1ee3f2aa0ed6af88893
Siddharth Pattnaik <sid.duduli@gmail.com>
2022-05-12 00:09:02 +0530

    jsx basic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+
+//you can use js in jsx inside curly brackets, only arguement or operators work
+const amIabove18=true;
+
 function App() {
     return <div>
         <Header/>
         <div className="content">
-            <p>This is content</p>
+            <p>This is content {2+2} {2**2}</p>
+            {amIabove18?<p>Kill me</p>:<p>Don't</p>}
         </div>
         <Footer/>
 
