6a09f1c41470669bfa86877ba040e476d7dba2a9
Siddharth Pattnaik <sid.duduli@gmail.com>
2022-05-11 18:18:15 +0530

    createRoot in React 18

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 
 import reportWebVitals from './reportWebVitals';
 
+const container=document.getElementById('root')
 
+const root=ReactDOM.createRoot(container)
+root.render( <h1>Hello world</h1>)
 
-ReactDOM.render(
+/*ReactDOM.render(
   <h1>Hello world</h1>,
   document.getElementById('root')
   
-);
+);*/
 
 
 reportWebVitals();
